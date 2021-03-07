db60bd36611ad3663bf3c6baa4dac6778957986a
Frank Ridder <fr.ridder@hotmail.com>
2021-03-07 16:20:02 +0100

    added small todo

--- processor_behavoir.vhd
+++ processor_behavoir.vhd
@@ -1,6 +1,27 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
-ARCHITECTURE behavior OF mips_processor  IS
+ARCHITECTURE behaviour OF mips_processor  IS
+/** Needed internal memory
+16 internal regesters ?
+conditonal codes
+program counter
+imm
+lo -Used in div and mult
+hi -Used in div and mult
+**/
 
+/**Needed funtions
+	Readmem
+	Writemem
+	Read to internal reg
+	Write to internal reg
+	Clear / set cc (condition codes)
+**/
 
+/**Processor loop:
+	Read instruction from mem
+	Decode instruction
+	Do instruction 
+**/
+END behaviour;
