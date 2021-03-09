3b4faef2963c1ae4a9967eb487a47126a08c81c2
Frank <fr.ridder@hotmail.com>
2021-03-09 16:15:37 +0100

    Rename

--- processor_behaviour.vhd
+++ processor_behaviour.vhd
@@ -2,13 +2,13 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ARCHITECTURE behaviour OF mips_processor  IS
-/** Needed internal memory
-16 internal regesters ?
-conditonal codes
-program counter
-imm
-lo -Used in div and mult
-hi -Used in div and mult
+/**Needed internal memory
+	16 internal regesters ?
+	conditonal codes
+	program counter
+	imm
+	lo -Used in div and mult
+	hi -Used in div and mult
 **/
 
 /**Needed funtions
